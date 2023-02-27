0777df1a26f50049ac104514e160ad1f0eb37b70
lgraul87 <lgraul87@gmail.com>
2023-02-28 00:14:39 +0100

    init db

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -2,53 +2,12 @@ import { DecimalPipe } from '@angular/common';
 import { Component, OnInit, PipeTransform } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { map, Observable, startWith } from 'rxjs';
-import {  Firestore } from '@angular/fire/firestore';
+import {  collectionData, Firestore } from '@angular/fire/firestore';
+import { collection } from 'firebase/firestore';
 
 
-interface Country {
-	name: string;
-	flag: string;
-	area: number;
-	population: number;
-}
 
-const COUNTRIES: Country[] = [
-	{
-		name: 'Russia',
-		flag: 'f/f3/Flag_of_Russia.svg',
-		area: 17075200,
-		population: 146989754,
-	},
-	{
-		name: 'Canada',
-		flag: 'c/cf/Flag_of_Canada.svg',
-		area: 9976140,
-		population: 36624199,
-	},
-	{
-		name: 'United States',
-		flag: 'a/a4/Flag_of_the_United_States.svg',
-		area: 9629091,
-		population: 324459463,
-	},
-	{
-		name: 'China',
-		flag: 'f/fa/Flag_of_the_People%27s_Republic_of_China.svg',
-		area: 9596960,
-		population: 1409517397,
-	},
-];
 
-function search(text: string, pipe: PipeTransform): Country[] {
-	return COUNTRIES.filter((country) => {
-		const term = text.toLowerCase();
-		return (
-			country.name.toLowerCase().includes(term) ||
-			pipe.transform(country.area).includes(term) ||
-			pipe.transform(country.population).includes(term)
-		);
-	});
-}
 
 
 @Component({
@@ -58,27 +17,25 @@ function search(text: string, pipe: PipeTransform): Country[] {
 	providers: [DecimalPipe]
 })
 export class BodyComponent implements OnInit {
-	countries$: Observable<Country[]>;
-	filter = new FormControl('', { nonNullable: true });
 
+
+	
 	profiles: any;
 
 	constructor(
-		pipe: DecimalPipe,
+
 		private firestore: Firestore) {
 
-		this.countries$ = this.filter.valueChanges.pipe(
-			startWith(''),
-			map((text) => search(text, pipe)),
-		);
+		
+			
 	}
 
 	ngOnInit(): void {
 	
-		// const profiles = collection(this.firestore, "user");
-		// collectionData(profiles).subscribe((data: any) => {
-		// 	this.profiles = data;
-		// });
+		const profiles = collection(this.firestore, "user");
+		collectionData(profiles).subscribe((data: any) => {
+			this.profiles = data;
+		});
 
 	}
 
