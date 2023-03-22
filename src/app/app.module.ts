1782ae9941a30f88a06ccfd986edf75ff1dccdeb
lgraul87 <lgraul87@gmail.com>
2023-03-22 21:35:29 +0100

    Subiendo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule, NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { BodyComponent } from './body/body.component';
 import { AsyncPipe, DecimalPipe, NgFor } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import {Firestore} from '@angular/fire/firestore';
-import { LoginComponent } from './login/component/login.component';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    BodyComponent,
-    FooterComponent,
-    HeaderComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -32,7 +23,6 @@ import { LoginComponent } from './login/component/login.component';
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideFirestore(() => getFirestore())
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
